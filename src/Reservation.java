40e4857e03fb4aa5e100773d368c824897d6f652
karimqalash <kekoessam.ke@gmail.com>
2020-12-17 18:20:53 +0200

    added reservation

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,17 +1,33 @@
+import java.time.LocalDate; // import the LocalDate class
+
 public class Reservation {
     private static int reservationCount=0;
     private int duration;
     private int id;
+    private LocalDate dateCreated;
     private Room room;
     private Customer customer;
     private Employee employee;
 
     public Reservation(int duration, Room room, Customer customer, Employee employee) {
         this.id=reservationCount;
+        this.dateCreated = LocalDate.now();
         this.duration=duration;
         this.room=room;
         this.customer=customer;
         this.employee=employee;
         reservationCount++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int reservationCount() {
+        return reservationCount;
+    }
+
+    public void print() {
+        System.out.printf("id :%d, date created: %s, duration: %d, customer: %s, employee: %s \n", this.id,this.dateCreated, this.duration, this.customer.getName(), this.employee.getName());
     }
 }
