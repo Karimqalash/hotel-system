5027e4b1a633d2566366665430af916b591536e4
karimqalash <kekoessam.ke@gmail.com>
2020-12-17 17:57:35 +0200

    added reservation

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,3 +1,17 @@
 public class Reservation {
-    
+    private static int reservationCount=0;
+    private int duration;
+    private int id;
+    private Room room;
+    private Customer customer;
+    private Employee employee;
+
+    public Reservation(int duration, Room room, Customer customer, Employee employee) {
+        this.id=reservationCount;
+        this.duration=duration;
+        this.room=room;
+        this.customer=customer;
+        this.employee=employee;
+        reservationCount++;
+    }
 }
